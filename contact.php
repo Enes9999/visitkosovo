677ba1535d666500784b727b546db8f32bbb39f8
Arta Mjeku <artaamjeku@gmail.com>
2020-09-04 20:05:27 +0200

    deletion

--- contact.php
+++ contact.php
@@ -1,42 +1,44 @@
-<?php
+<?php 
+$page = 'contact';
+require_once "./header.php"; ?>
 
-session_start();
 
-if(isset($_SESSION['contactus'])){
-    header("Location: HomePage.php");
+<div
+    style="margin-top: -150px; margin-bottom: -50px; background:url( https://ceed-global.org/wp-content/uploads/2020/03/Kosovo.png); height: 500px; max-width: 100%;">
+    <div>
+        <div class="text-wrapper"
+            style=" flex-wrap: wrap;  margin-top: 200px;  margin-bottom: -250px; width: 100%; height: 75vh; padding-top: 40px;">
+            <h1 style="color: white; line-height: 50px; margin-bottom: 100px;">Have any questions for us? </h1>
+            <p style="color: white; margin: 0 auto;">Write them down below.</p>
+        </div>
+    </div>
+</div>
+<div class="contact-section">
+    <div class="inner-width">
+        <h1 style="margin-top: 60px;">Get in touch with us</h1>
 
-    //beje kete veprim nese eshte i loguar user
-}
+        <form name="forma" method="POST" action="contact.php" onsubmit="return validate(event)">
+            <div class="fields">
 
-require 'CRUD/includes/dbconnect.php';
-//lidhja me databaze
+                <input type="text" placeholder="Name" class="name" id="name" name="emri">
+                
+                <input type="text" placeholder="Your Email" class="email" id="email" name="email">
+                
+                <input type="tel" placeholder="Phone Number" class="number" id="number" name="numri">
+                
 
-if(isset($_POST['submit'])){
-    $emri = $_POST['emri'];
-    $email = $_POST['email'];
-    $numri = $_POST['numri'];
-    $msg = $_POST['msg'];
-    $message = '';
-    $sql = 'INSERT INTO contactus (emri, email, numri, msg) VALUES (:emri, :email, :numri, :msg)';
-    //kete e bojme sepse te lidhja e kemi perdor pdo 
-    $query = $pdo->prepare($sql);
-    //kete e bojme qe mos me ja leju dikujt qe ne vend te imelles me shkru emrin e kshtu me radhe
-    $query->bindParam('emri', $emri);
-    $query->bindParam('email', $email);
-    $query->bindParam('numri', $numri);
-    $query->bindParam('msg', $msg);
-    //ekzekutimi i query
-   if($query->execute()){
-    header("Location: ContactUs.php");
-   }else{
-       $message = "Ka probleme gjate regjistrimit ";
-   }
+                <textarea type="text" rows="1" placeholder="Message" class="message" id="msg" name="msg"></textarea>
+                
+            </div>
 
-   }    
-   ?>
+            <button type="submit" id="send" value="Send" name="submit">SUBMIT</button>
+            
+        </form>
 
-   <?php if(!empty($message)){
-       echo $message;
-   }
+            
+            
+        
+    </div>
+</div>
 
-   ?>
+<?php require_once "./footer.php"; ?>
--- home.php
+++ home.php
@@ -18,29 +18,27 @@ require_once "./header.php";
 }
 </style>
 
-
-<div class="wrap">
-    <div id="arrow-left" class="arrow"></div>
-    <div id="slider">
-        <div class="slide slide1">
-            <div class="slide-content">
-                <span>Visit Kosovo</span>
-            </div>
-        </div>
-        <div class="slide slide2">
-            <div class="slide-content">
-                <span>Visit Kosovo</span>
-            </div>
-        </div>
-        <div class="slide slide3">
-            <div class="slide-content">
-                <span>Visit Kosovo</span>
-            </div>
-        </div>
+<!-- slideri -->
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="https://wallpapercave.com/wp/wp2020983.jpg">
+      </div>
+      <div class="swiper-slide">
+        <img src="images\kosovolandscape.png">
+      </div>
+      <div class="swiper-slide">
+        <img src="images\kosovo10.jpg">
+      </div>
     </div>
-    <div id="arrow-right" class="arrow"></div>
-</div>
-</div>
+    <!-- Add Pagination -->
+    <div class="swiper-pagination" style="padding-top:2000px;"></div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+
+<!-- sideri -->
 <h1 style="text-align: center; font-family: 'Mansalva',cursive; font-weight: 300; font-size: 50px; margin-top: 60px; ">
     Welcome to Kosovo</h1>
 <h2 style="text-align: center; font-family: 'DM Sans', sans-serif; font-size: 20px;"> Useful information to visit
@@ -67,47 +65,8 @@ require_once "./header.php";
 
 
 
-<!--<div style="position: relative; width: 100%; margin: 0 auto;">
-
-		<img src="images/https://upload.wikimedia.org/wikipedia/commons/9/91/Panorama_of_Brezovica%2C_%C5%A0trpce%2C_Kosovo.jpg" style="width: 100%; height: 650px;">
-
-			<div style="position: absolute; 
-						bottom: 0; 
-						background: rgb(0, 143, 24); 
-						background: rgba(0, 0, 0, 0.7); 
-						color: #FFFFFF; 
-						width: 100%; 
-						padding: 20px;
-						background:url(https://blog.hslu.ch/majorobm/files/2019/03/Pamja-Bjelluhes-nga-kurrzi-i-liqenatit.jpg);
-						">
-
-				<p style="font-size: 50px; text-align: center; color: white; font-weight: bolder;">Europe's youngest country<p>
-   				<p style="text-align: center; width: 1000px; margin: 0 auto; color: silver;">Europe's newest country, Kosovo is a fascinating land at the heart of the Balkans rewarding visitors with welcoming smiles, charming mountain towns, incredible hiking opportunities and 13th-century domed monasteries brushed in medieval art – and that's just for starters. </p>
-			</div>
-	</div>-->
 
 
-
-
-
-<!--<div class="part-3">
-		<div style="position: relative; width: auto;height: 50%; margin: 0 auto; margin-bottom: 20px;">
-
-			<img src="images/kosovo10.jpg" style="width: 90%; height:50%">
-	
-				<div style="position: absolute; 
-							bottom: 0; 
-							background: rgb(0, 143, 24); 
-							background: rgba(0, 0, 0, 0.7); 
-							color: #FFFFFF; 
-							width: 90%;">
-	
-					<p style="font-size: 50px; text-align: center; color: white; font-weight: bolder; line-height: normal; font-size: 50px; font-family: 'DM Sans', sans-serif; margin-bottom: 0; ">Where is Kosovo on the map?<p>
-					   <p style="text-align: center; width: auto; margin: 0 auto; color: silver; line-height: normal; font-size: 30px; font-family: 'DM Sans', sans-serif;">Kosovo is located on the Southeastern part of Europe sharing borders with Albania, Macedonia, Montenegro and Serbia.</p>
-				</div>
-		</div>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1504722.053089399!2d19.780976728476453!3d42.55719838546098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13537af354bf7df1%3A0xbfffeedfabc31791!2sKosovo!5e0!3m2!1sen!2s!4v1589859895128!5m2!1sen!2s" id="map" style="width: 100%; height:515px; " frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-	</div>-->
 
 <div
     style="margin-top: -150px; font-family: 'DM Sans', sans-serif; background:url( https://i.pinimg.com/originals/bb/3a/38/bb3a38ba9dbc79f95f553094f1e63033.jpg); height: 855px; max-width: 100%;">
